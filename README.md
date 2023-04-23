1301453616afb481f38e2d253378a69976fdb4dc
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-23 11:21:41 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Ask the user for the filename. If no parameters given, filename will default to mylife.txt
+    - If a file already exists, open that file.
+    - If the file is new, create a new one.
+2. Ask the user for input, and then append that input to the file.
---
+++ file_writer.py
@@ -0,0 +1,12 @@
+'''
+filename: file_writer.py
+
+This program will ask the user for sentences to write to a file.
+If the user has no more sentences to add, the program terminates.
+
+1. Ask the user for the filename. If no parameters given, filename will default to mylife.txt
+    - If a file already exists, open that file.
+    - If the file is new, create a new one.
+2. Ask the user for input, and then append that input to the file.
+
+'''
